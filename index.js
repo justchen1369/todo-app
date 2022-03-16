c25b0bfbfdb6496f8f2af9c4916f515cd3b18b77
justchen1369 <justchenabc@gmail.com>
2022-03-16 18:55:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+const sqlite = require('better-sqlite3');
+const express = require('express');
+const blocked = require('blocked');
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Database
+const dbPath = process.env.DATABASEPATH || './db.sqlite';
+
+const db = sqlite(dbPath, { verbose: console.log });
+
+
+
+db.exec(
+    `CREATE TABLE IF NOT EXISTS todo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        timestamp INTEGER,
+        description TEXT,
+        done INTEGER
+    );`
+)
+
+app.get('/todos/range', (req, res) => {
+    const { start, end } = req.query;
+
+    if (!start || !end) {
+        res.status(400).send({ error: 'Missing start or end parameter'});
+        return;
+    }
+
+    const result = db.prepare(`SELECT * FROM todo WHERE timestamp BETWEEN ? AND ?`).all(start, end);
+    res.send(result);
+})
+
+app.get('/todos/all', (req, res) => {
+    const result = db.prepare(`SELECT * FROM todo`).all();
+    res.send(result);
+})
+
+app.post('/create', (req, res) => {
+    const { description } = req.body;
+
+    console.log(req.body);
+    if (!description) {
+        res.status(400).send({ error: 'Missing description' });
+        return;
+    }
+
+    const timestamp = Date.now();
+    
+    db.prepare('INSERT INTO todo (timestamp, description, done) VALUES (?, ?, ?)').run(timestamp, description, 0);
+    
+    const id = db.prepare('SELECT id FROM todo WHERE timestamp = ?').get(timestamp);
+    res.send({timestamp, description, id: id.id});
+})
+
+app.patch('/done', (req, res) => {
+    const { id } = req.body;
+    
+    const done = req.body.done ? 1 : 0;
+
+    console.log({id, done});
+    if (done !== 0 && done !== 1) {
+        res.status(400).send({ error: 'done must be 0 or 1' });
+        return;
+    }
+
+    if (!id) {
+        res.status(400).send({ error: 'Missing id' });
+        return;
+    }
+
+    if (!db.prepare('SELECT * FROM todo WHERE id = ?').get(id)) {
+        res.status(404).send({ error: 'Todo not found' });
+        return;
+    }
+
+    db.prepare(`UPDATE todo SET done = ? WHERE id = ?`).run(done, id);
+
+    res.send({ id });
+})
+
+app.patch("/description", (req, res) => {
+    const { id, description } = req.body;
+
+    if (!id) {
+        res.status(400).send({ error: 'Missing id' });
+        return;
+    }
+
+    if (!description) {
+        res.status(400).send({ error: 'Missing description' });
+        return;
+    }
+
+    if (!db.prepare('SELECT * FROM todo WHERE id = ?').get(id)) {
+        res.status(404).send({ error: 'Todo not found' });
+        return;
+    }
+
+    db.prepare(`UPDATE todo SET description = ? WHERE id = ?`).run(description, id);
+
+    res.send({ id });
+})
+// using app.delete hangs for some reason
+app.delete("/delete", (req, res) => {
+    const { id } = req.body;
+    
+    if (!id) {
+        res.status(400).send({ error: 'Missing id'});
+        return;
+    }
+
+    if (!db.prepare('SELECT * FROM todo WHERE id = ?').get(id)) {
+        res.status(404).send({ error: 'Todo not found'});
+        return;
+    }
+
+    db.prepare(`DELETE FROM todo WHERE id = ?`).run(id);
+
+    res.send("OK");
+})
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ public/index.js
@@ -0,0 +1,176 @@
+// @ts-check
+
+function restMethod(method, url, data) {
+    return fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(async response => {
+
+        let json
+        try {
+            json = await response.json();
+        } catch (e) { }
+
+        return {
+            status: response.status,
+            json: json,
+            message: response.statusText
+        }
+    });
+}
+
+const todos = document.getElementById("todos");
+
+async function createTodo(description) {
+    // @ts-ignore
+
+    if (!description) {
+        return
+    }
+
+    const { id } = (await restMethod("POST", "/create", { description })).json
+
+    const container = createTodoItem(description, id);
+
+    const todoItem = container.querySelector("li");
+
+    todoItem.addEventListener("focusout", async () => {
+        const description = todoItem.innerText;
+        const res = await restMethod("PATCH", "/description", { id, description });
+
+        if (res.status !== 200) {
+            error(res.status, res.message);
+            return;
+        }
+    })
+
+    todos.appendChild(container);
+
+    return container;
+}
+
+function createTodoItem(description, id, done = false) {
+    const container = document.createElement("div");
+    container.classList.add("todo-item");
+
+    // clickable "done"
+    const doneButton = document.createElement("input");
+    doneButton.type = "checkbox";
+    doneButton.classList.add("done-button");
+    doneButton.innerText = "done";
+    doneButton.onclick = async () => {
+        const done = doneButton.checked;
+        
+        if (done) {
+            todoItem.classList.add("strike");
+        } else {
+            todoItem.classList.remove("strike");
+        }
+
+        const res = await restMethod("PATCH", "/done", { id, done });
+
+        if (res.status !== 200) {
+            error(res.status, res.message);
+            todoItem.classList.remove("strike");
+            return;
+        }
+    }
+    container.appendChild(doneButton);
+
+    const todoItem = document.createElement("li");
+    if (done) {
+        todoItem.classList.add("strike");
+        doneButton.checked = true;
+    }
+    todoItem.setAttribute("data-id", id);
+    todoItem.spellcheck = false;
+
+    // @ts-ignore
+    todoItem.contentEditable = true;
+
+    todoItem.textContent = description;
+
+    container.appendChild(todoItem);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("close");
+    deleteButton.innerHTML = '<img src="/trash-can.svg"></img>';
+    deleteButton.onclick = async () => {
+        await restMethod("DELETE", "/delete", { id });
+        container.remove();
+    }
+    container.appendChild(deleteButton);
+
+    // prevent newlines in the description
+    todoItem.addEventListener("keydown", e => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+        }
+    })
+    return container;
+}
+
+restMethod("GET", "/todos/all")
+    .then(async res => {
+        const todoList = (await res).json
+        todoList.forEach(todo => {
+            todos.appendChild(createTodoItem(todo.description, todo.id, todo.done));
+        });
+    })
+
+function error(code, message) {
+    console.log(`Error ${code}: ${message}`);
+}
+
+
+const newTodo = document.getElementById("new-todo");
+
+async function newTodoClickCallback() {
+}
+
+
+newTodo.addEventListener("click", async () => {
+    newTodo.style.visibility = "hidden";
+    let finished = false;
+    while (!finished) {
+        finished = true;
+        const container = createTodoItem("", "", false);
+
+        todos.appendChild(container);
+        container.querySelector("li").focus();
+
+
+        
+        await new Promise((resolve) => {
+            async function attemptCreateTodo() {
+                container.removeEventListener("keydown", containerKeyDownCallback);
+                container.removeEventListener("focusout", containerFocusOutCallback);
+                if (container.querySelector("li").innerText.length > 0) {
+                    container.replaceWith(await createTodo(container.querySelector("li").innerText));
+                } else {
+                    container.remove();
+                }
+            }
+
+            async function containerKeyDownCallback(e) {
+                if (e.key === "Enter") {
+                    attemptCreateTodo();
+                    finished = false;
+                    resolve();
+                }
+            }
+            container.addEventListener("keydown", containerKeyDownCallback);
+
+            async function containerFocusOutCallback() {
+                attemptCreateTodo();
+                resolve();
+            }
+            container.addEventListener("focusout", containerFocusOutCallback);
+
+        })
+    }
+    newTodo.style.visibility = "visible";
+});
